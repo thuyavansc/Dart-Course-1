911322d8bd13f7f22083612fbdc588dd51016644
Kingston <Kingston@DESKTOP-MVP19NT>
2024-04-03 15:46:27 +0530

    String and raw string

--- playground.dart
+++ playground.dart
@@ -1,31 +1,18 @@
 import 'dart:io';
 
 void main() {
-  //INT
-  int amount1 = 100;
-  var amount2 = 200;
-  var amountT = amount1 + amount2;
-  print('Amount 1 : $amount1 | Amount2: $amount2 \n');
+  var s1 = 'Single quotes work well for string literals.';
+  var s2 = "Double Quotes work just as will.";
+  var s3 = 'It\'s easy to escape the string delimiter.';
+  var s4 = "It's even easier to use other delimter";
 
-  //Double
-  double dAmount1 = 100.11;
-  var dAmount2 = 200.22;
-  print('dAmount1: $dAmount1 | dAmount2: $dAmount2\n');
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  print('');
 
-  //String
-  String name1 = 'Mark';
-  var name2 = 'Antony';
-  print('Name1 : $name1 | Name2: $name2 \n');
-
-  //Boolean
-  bool isTrue1 = true;
-  var isTure2 = false;
-  print('isTrue1 : $isTrue1 | isTure2: $isTure2 \n');
-
-  //Dynamic
-  dynamic weakVariable = 100;
-  print('This is the weak variable 1  - $weakVariable');
-
-  weakVariable = 'This is Dart Programming';
-  print('This is the weak variable 2  - $weakVariable');
+  //Raw String
+  var s = r'In a raw String , not even \n gets special treatment.';
+  print(s);
 }

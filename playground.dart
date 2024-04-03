4740c2f507fccdf45f49378d28e4114cd2b6cf2f
Kingston <Kingston@DESKTOP-MVP19NT>
2024-04-03 15:59:09 +0530

    multi-line String and Interpolation

--- playground.dart
+++ playground.dart
@@ -1,18 +1,18 @@
 import 'dart:io';
 
 void main() {
-  var s1 = 'Single quotes work well for string literals.';
-  var s2 = "Double Quotes work just as will.";
-  var s3 = 'It\'s easy to escape the string delimiter.';
-  var s4 = "It's even easier to use other delimter";
+  var age = 35;
+  var str = 'My age is : $age';
+  print(str);
+
+  var s1 = '''
+you can create multi-line String like this one.
+''';
+
+  var s2 = """
+This is also a multi-line String.
+""";
 
   print(s1);
   print(s2);
-  print(s3);
-  print(s4);
-  print('');
-
-  //Raw String
-  var s = r'In a raw String , not even \n gets special treatment.';
-  print(s);
 }
